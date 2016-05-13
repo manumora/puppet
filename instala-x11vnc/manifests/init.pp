da86f1ac230cd473af7c08cc89da227085fbe16b
Manu <manuito@gmail.com>
2016-05-13 12:23:07 +0200

    new file:   instala-x11vnc.tar.gz 	deleted:    instala-x11vnc.zip 	modificado: instala-x11vnc/files/x11vnc 	new file:   instala-x11vnc/files/x11vnc.conf 	modificado: instala-x11vnc/manifests/init.pp 	new file:   siatic_chapuza_resolucion.tar.gz 	new file:   siatic_chapuza_resolucion/files/resolutions_1.4-1_i386.deb 	new file:   siatic_chapuza_resolucion/manifests/init.pp

--- instala-x11vnc/manifests/init.pp
+++ instala-x11vnc/manifests/init.pp
@@ -1,22 +1,3 @@
-##############################################################################
-# Project:     Instala x11vnc
-# Purpose:     Puppet task
-# Date:        28-Jan-2016.
-# Author:      Manuel Mora Gordillo
-# Copyright:   2016 - Manuel Mora Gordillo    <manuel.mora.gordillo @nospam@ gmail.com>
-#
-# Instala x11vnc is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# Instala x11vnc is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with Instala x11vnc. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 class instala-x11vnc {
 
@@ -25,6 +6,12 @@ class instala-x11vnc {
 		file {"/etc/x11vnc.pass":
 			owner => root, group => root, mode => 600,
 			source => "puppet:///modules/instala-x11vnc/x11vnc.pass",
+		}
+
+		file {"/etc/init/x11vnc.conf":
+			owner => root, group => root, mode => 755,
+			source => "puppet:///modules/instala-x11vnc/x11vnc.conf",
+			require => Package["x11vnc"],
 		}
 
 		file {"/etc/init.d/x11vnc":
@@ -37,6 +24,6 @@ class instala-x11vnc {
 			path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 			command => "chmod +x /etc/init.d/x11vnc; update-rc.d x11vnc defaults 99; service x11vnc start",
 			unless => "ls /etc/rc*.d| grep x11vnc",
-			require => File["/etc/init.d/x11vnc", "/etc/x11vnc.pass"],
+			require => File["/etc/init.d/x11vnc", "/etc/x11vnc.pass", "/etc/init/x11vnc.conf"],
 		}
 }
