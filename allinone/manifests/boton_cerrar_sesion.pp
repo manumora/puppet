a148ad48469a066917cdb2aec30b2b4ecf594866
Manu <manuel@fusiontelecom.co>
2024-10-15 12:16:31 +0200

    Fixed onlyif bug

--- allinone/manifests/boton_cerrar_sesion.pp
+++ allinone/manifests/boton_cerrar_sesion.pp
@@ -22,26 +22,30 @@
 
 class allinone::boton_cerrar_sesion {
     exec { "copia_xfce-session-logout":
-        command => "/bin/cp /usr/bin/xfce4-session-logout /usr/bin/xfce4-session-logout.orig",
-        unless => "/usr/bin/test -f /usr/bin/xfce4-session-logout.orig",
+        command => "cp /usr/bin/xfce4-session-logout /usr/bin/xfce4-session-logout.orig",
+        path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+        unless => "test -f /usr/bin/xfce4-session-logout.orig",
         notify => Exec["instala_salida_automatica"]
     }
 
     exec { "instala_salida_automatica":
-        command => "/bin/echo '#!/bin/bash \n xfce4-session-logout.orig --logout' > /usr/bin/xfce4-session-logout",
-        onlyif => "/usr/bin/test -f /usr/bin/xfce4-session-logout.orig",
-        unless => "/bin/grep '--logout' /usr/bin/xfce4-session-logout",
+        command => "echo '#!/bin/bash \n xfce4-session-logout.orig --logout' > /usr/bin/xfce4-session-logout",
+        path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+        onlyif => "test -f /usr/bin/xfce4-session-logout.orig",
+        unless => "grep -- '--logout' /usr/bin/xfce4-session-logout",
         notify => Exec["cambia_icono_logout"]
     }
 
     exec { "cambia_icono_logout":
-        command => "/bin/sed -i 's/+logout/-logout/' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml",
-        unless => "/bin/grep '\"-logout\"' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml",
+        command => "sed -i 's/+logout/-logout/' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml",
+        path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+        unless => "grep '\"-logout\"' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml",
         notify => Exec["nuevo_icono_logout-dialog"]
     }
 
     exec { "nuevo_icono_logout-dialog":
-        command => "/bin/sed -i 's/-logout-dialog/+logout-dialog/' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml",
-        unless => "/bin/grep '\"+logout-dialog' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml",
+        command => "sed -i 's/-logout-dialog/+logout-dialog/' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml",
+        path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+        unless => "grep '\"+logout-dialog' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml",
     }
 }
